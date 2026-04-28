6e726f093b0f8ce455b75e0e04f4a4906d2d0fa6
claire681 <clairekemanzi01@gmail.com>
2026-04-27 18:52:21 -0700

    fix backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL
-  ? `${process.env.REACT_APP_API_URL}/api/v1`
-  : '/api/v1';
+const API_BASE = "http://18.117.114.172:8000/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE,
