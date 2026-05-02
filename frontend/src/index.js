2b99909d919b6822c4abfbb6e24fd0e573cc8e88
claire681 <clairekemanzi01@gmail.com>
2026-05-02 01:12:59 -0700

    Fix index.js AIProvider import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AIProvider } from './hooks/useAI';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AIProvider>
-      <App />
-    </AIProvider>
+    <App />
   </React.StrictMode>
 );
 
