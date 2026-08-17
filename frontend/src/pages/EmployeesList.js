749549aa5c698208f200e67c6bcec2a867703ddb
Claire <claire681@users.noreply.github.com>
2026-08-17 16:25:04 +0000

    privacy lock icon color to dark

--- frontend/src/pages/EmployeesList.js
+++ frontend/src/pages/EmployeesList.js
@@ -28,7 +28,7 @@ const SUCCESS_SOFT = "#DCFCE7";
 
 const PRIVACY_KEY = "novala_privacy";
 const PRIVATE_MASK = (
-  <span style={{ display: "inline-flex", alignItems: "center", color: "#9CA3AF" }}>
+  <span style={{ display: "inline-flex", alignItems: "center", color: "#12262B" }}>
     <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor" stroke="none"><path d="M18 8h-1V6a5 5 0 0 0-10 0v2H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2zm-9-2a3 3 0 0 1 6 0v2H9V6zm4 10h-2v-3h2v3z"/></svg>
   </span>
 );
